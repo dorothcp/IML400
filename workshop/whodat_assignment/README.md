72ab3f6dda89ef469a6998694f44e4583f53ff89
dorothcp <dorothcp@usc.edu>
2016-02-05 09:59:53 -0800

    finish whodat assignment

---
+++ workshop/whodat_assignment/README.md
@@ -0,0 +1,94 @@
+# Whodat Assignment
+
+### 1. Setup  
+To do this you need to update your __CLASS__ repository.  
+1. Go to your directory that has the class repository   
+2. Enter:   
+
+```
+//make sure that you are not on the branch for the course website  
+git checkout master  
+// Now grab all the files that I added and download them to your machine  
+git pull
+```
+
+this will pull the new files from the repository to your computer. You will find the files that you need in this folder:
+
+```
+workshops/whodat_assignment
+```
+
+Now, in Finder or Windows Exlporer you are going to add the assignment folder to your class website. You should have a workshops folder for your class website. (If not refer to the syllabus instructions again to remind yourself about the directory structure of your website. ) Copy the _whodat\_assignment_ folder into the _workshop_ directory of your website. You probably should also add an index file to the _workshop_ directory with a link to the _whodat\_assignment_ directory.
+
+Go into that directory.  
+(---sidebar---- Rememeber the linux command to change directories??)
+
+```
+cd
+```
+
+shortcut to go to the parent directory of the directory that you are in:  
+
+```
+cd ..
+```
+--- end sidebar ---)
+
+
+Go to the assignment folder in your class repository with Terminal. From inside the _whodat\_assignment folder enter:
+
+```
+npm start
+```
+
+this will start your browser-sync session and open your default web browser. Make sure that you are using Chrome. From here you can open the Chrome Web Developer console and start working.
+
+To set up your Chrome Web Developer Session as an editor:
+Follow the instructions here:  
+[Setup](https://developer.chrome.com/devtools/docs/workspaces)
+
+follow the instructions for: 'Add local source files to workspace'
+
+
+### 2. Assignment
+I am giving you a series of "challenges" of increasing difficulty. Accomplish as many as you can and we will review then in class. See below for handin instructions.
+
+#### Workshop Process  
+If you look at _main.js_ inside of _whodat\_assignment/public/scripts_ you willl see that there are commented sections that describe the challenge. There is also a section for you to paste your code.  
+
+I would suggest that you use an iterative process like the one that we used in class. That is:
+
+1. Use the console in Chrome Developer to enter commands and test the results.  
+2. Once you have a series of commands that work then you can cut and paste the code that you entered into _main.js_.  
+3. When you do this the next time you reload the webpage the script in _main.js_ will automatically run.  
+
+Do the best you can. I expect that you may struggle with some or all of this. Don't get frustrated, or at least keep your frustration in perspective: learning only happens when you get unstuck. But to get unstuck (and therefore learn) you first have to get stuck. Alright I ain't a poet...and I know it...(ha!) but just chill, try to have fun with it and realize that by next week this time this assignment will be easy for all of you once we review your attempts in class.  
+
+
+#### Workshop Resources
+To find help on the different methods that you can call and what they do go here:  
+
+##### Setting Attributes
+* [SetAttributeMethod](https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute)
+
+##### Accessing Elements on the page
+There are different methods to do this as we explored in class. Feel free to use any that you are comfortable with. __Rememeber__ that if the method returns a list you will have to access the list item that you want!  
+
+* [getElementById](https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById)  
+* [getElementsByClassName](https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByClassName)  
+* [getElementsByTagName](https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByTagName)  
+* [querySelector](https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector)  
+* [querySelectorAll](https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll)  
+
+
+#### Workshop Handin instructions  
+Remember that in order to see your assignment on your website you will have to run: 
+
+```
+git add .
+git commit -m"some message"
+git push
+```
+
+You will enter that command from inside your class repository.  
+
---
+++ workshop/whodat_assignment/main.js
@@ -0,0 +1,157 @@
+/* ----- */
+// Replace YOUR NAME with yours
+
+var NAME = "Dorothy Peng";
+console.log( "/*****************/\n" + NAME +"'s submission \nfor Whodat Assignment" + "\n/*****************/\n");
+
+
+
+/*
+ * CHALLENGES
+ *
+ * Complete as many of the following challenges 
+ * that you can. Simply put your code where indicated
+ * and when you have completed a challenge
+ * the delete the not in the 'not completed' message
+ * if you try and don't get it then change the log message to 
+ * something else. (e.g. 'attempted', 'attempted but javascript stinks', 
+ * 'attempted but the instructor stinks', ...)
+ */
+
+
+
+
+/* -=-=-=-			=-=-=-=-=			-=-=-= 	*/
+/*  Ch1											*/	
+/*	Change the color of the wings on  			*/	
+/*	1st owl to a different color				*/	
+/* -=-=-=-			=-=-=-=-=			-=-=-= 	*/
+console.log('...challenge 1...');
+
+//paste your code here...and delete
+var leftWing= document.getElementsByClassName("wing_L");
+leftWing[0].style.backgroundColor= "red";
+
+/* -=-=-=-			=-=-=-=-=			-=-=-= 	*/
+/*  Ch2											*/	
+/*	Change the background color of the cage  	*/	
+/*	on the 1st row at the end					*/	
+/* -=-=-=-			=-=-=-=-=			-=-=-= 	*/
+console.log('...challenge 2...');
+var cage3= document.getElementsByClassName("cage  column-4");
+cage3[2].style.backgroundColor= "green";
+
+/* -=-=-=-			=-=-=-=-=			-=-=-= 	*/
+/*  Ch3											*/	
+/*	Change the length of the beak for the owl	*/	
+/*	at the end of the second row				*/	
+/* -=-=-=-			=-=-=-=-=			-=-=-= 	*/
+
+/** 		HINT		 **/
+// to change the length of the beak you need to change
+// the pixel value of:
+// border-top
+// e.g.
+// changing:
+// border-top: 60px solid goldenrod;
+// to
+// border-top: 120px solid goldenrod;
+// would double the length of the beak.
+/** ---		----	---	 **/
+
+console.log('...challenge 3...');
+var beak= document.getElementsByClassName("beak");
+beak[5].style.borderTop= "120px solid goldrenrod";
+
+
+
+
+/* -=-=-=-			=-=-=-=-=			-=-=-= 	*/
+/*  Ch4											*/	
+/*	Write a function that takes a number		*/	
+/*  and returns the cage on the page that 		*/
+/* 	corresponds to that number 					*/
+/* 		e.g. calling 							*/
+/*				getCage(0)						*/
+/*		would return <section class="cage" ...	*/
+/*					 </section>					*/
+/*												*/					
+/*				html Tag for the first cage     */					
+/* -=-=-=-			=-=-=-=-=			-=-=-= 	*/
+console.log('...challenge 4...');
+function getCage(cageNumber) {
+	var selectedCage= document.getElementsByClassName("cage  column-4");
+	return selectedCage(cageNumber);
+}
+
+
+
+
+/* -=-=-=-			=-=-=-=-=			-=-=-= 	*/
+/*  Ch5											*/	
+/*	Change the colors of the cages and owls		*/	
+/*	in the middle column to be the same			*/
+/*												*/	
+/*												*/	
+/*  HINTkey: "value", 							*/
+/*  If you got Ch4 then you should be able to 	*/
+/* 	you should be able to use that function to 	*/
+/*	assist you 									*/
+/* -=-=-=-			=-=-=-=-=			-=-=-= 	*/
+console.log('...challenge 5...');
+console.log('\tnot completed')
+//paste your code here...
+function changeColor (owlNum) {
+	var cage= document.getElementsByClassName("cage  column-4");
+	var body= document.getElementsByClassName("body prpl-d4");
+	cage[owlNum].style.backgroundColor= "blue";
+	body[owlNum].style.backgroundColor= "pink";
+}
+changeColor(4);
+changeColor(1);
+
+
+/* -=-=-=-			=-=-=-=-=			-=-=-= 	*/
+/*  Ch6											*/	
+/*	Write functions that take an owl element	*/	
+/* 	as an argument and return the html sections */
+/*	of different body parts						*/
+/*												*/	
+/*	see functions below for hints				*/	
+/*												*/	
+/* -=-=-=-			=-=-=-=-=			-=-=-= 	*/
+console.log('...challenge 6...');
+console.log('\tnot completed')
+//paste your code here...
+
+function getWings(anOwl) {
+	var wingsL= document.getElementsByClassName("wing_L");
+	var wingsR= document.getElementsByClassName("wing_R");
+	return [wingsR[anOwl], wingsL[anOwl];
+}
+getWings(3);
+
+function getBeak(anOwl) {
+	function getBeak (anOwl) {
+	var beak= document.getElementsByClassName("beak");
+	return beak[anOwl];
+}
+
+function getEyes(anOwl) {
+	var eyeL= document.getElementsByClassName("eye_L");
+	var eyeR= document.getElementsByClassName("eye_R");
+	return [eyeR[anOwl], eyeL[anOwl];
+}
+
+function getBody(anOwl) {
+	var body= document.getElementsByClassName("body prpl-d4");
+	return body[anOwl];
+}
+
+function getPupils(anOwl) {
+	var pupil= document.getElementsByClassName("pupil");
+	return [pupil[anOwl*2], pupil[anOwl*2+1]];
+}
+getPupils(5);
+
+
